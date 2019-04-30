fe8e45154305317c52b8d456c3c15ab8c20d8ec7
Vince94m <cypetdesigns@gmail.com>
2019-04-30 11:23:41 +0200

    Initial Upload

---
+++ precache-manifest.0dcfd6a9e031e3969b84e61d27e40baa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200d2b8cc09511e037b73ae54c0b90da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6f0841419812ac9cc932",
+    "url": "/static/css/main.8d74a574.chunk.css"
+  },
+  {
+    "revision": "763dc16c30accf0eb714",
+    "url": "/static/js/2.00212eb8.chunk.js"
+  },
+  {
+    "revision": "6f0841419812ac9cc932",
+    "url": "/static/js/main.49be5939.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "207bec2dde8b54c7e4dabdeef7579c2f",
+    "url": "/static/media/Catull.207bec2d.woff"
+  },
+  {
+    "revision": "3cd4abbd229f2b4da44ddb14577def38",
+    "url": "/static/media/pawprint.3cd4abbd.png"
+  }
+]);
