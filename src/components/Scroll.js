258376c2ba73cd8940bf5418def58f4ffca61879
Vince94m <cypetdesigns@gmail.com>
2019-05-03 09:45:32 +0200

    background fix

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -3,7 +3,7 @@ import './Scroll.css';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflowY: 'scroll', height: '500px', marginTop: '-60px'}}>
+		<div style={{overflowY: 'scroll', height: '900px', marginTop: '-60px'}}>
 			{props.children}
 		</div>
 	)
--- src/containers/App.js
+++ src/containers/App.js
@@ -43,7 +43,7 @@ class App extends React.Component{
           <CardList cats={filteredCats}/>
         </Scroll>
 
-        <div className='mt4'>logo downloaded from <a href="https://www.flaticon.com/"           
+        <div className='mt4 pa2'>logo downloaded from <a href="https://www.flaticon.com/"           
         title="Flaticon">www.flaticon.com</a>, licensed by 
         <a rel="noopener noreferrer" href="http://creativecommons.org/licenses/by/3.0/"
         title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
